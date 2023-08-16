b90c1f4061f601ea4dfa5c8ff528f4d9e705e293
SHAKOTN <shakotn@protonmail.com>
2023-08-16 16:22:37 +0300

    [tests] - finish basic setup by adding strategies to vault; also - add basic setup test

--- test/BaseFixture.t.sol
+++ test/BaseFixture.t.sol
@@ -8,6 +8,9 @@ contract BaseFixture is Test {
     ERC20 public constant THREE_POOL_TOKEN =
         ERC20(address(0x6c3F90f043a72FA612cbac8115EE7e52BDe6E490));
 
+    address public constant THREE_POOL =
+        0xbEbc44782C7dB0a1A60Cb6fe97d0b483032FF1C7;
+
     address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
     address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
     address public constant USDT = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
@@ -26,5 +29,51 @@ contract BaseFixture is Test {
         daiStrategy = new FluxStrategy(address(gVault), DAI, F_DAI);
         usdcStrategy = new FluxStrategy(address(gVault), USDC, F_USDC);
         usdtStrategy = new FluxStrategy(address(gVault), USDT, F_USDT);
+        gVault.addStrategy(address(daiStrategy), 3333);
+        gVault.addStrategy(address(usdcStrategy), 3333);
+        gVault.addStrategy(address(usdtStrategy), 3333);
+    }
+
+    /// @notice Basic test to ensure the fixture is working
+    function testBasicSetup() public {
+        assertEq(daiStrategy.asset(), DAI);
+        assertEq(usdcStrategy.asset(), USDC);
+        assertEq(usdtStrategy.asset(), USDT);
+
+        // Check allowances on 3pool:
+        assertEq(
+            ERC20(DAI).allowance(address(daiStrategy), THREE_POOL),
+            type(uint256).max
+        );
+        assertEq(
+            ERC20(USDC).allowance(address(usdcStrategy), THREE_POOL),
+            type(uint256).max
+        );
+        assertEq(
+            ERC20(USDT).allowance(address(usdtStrategy), THREE_POOL),
+            type(uint256).max
+        );
+
+        // Check allowances on fTokens:
+        assertEq(
+            ERC20(DAI).allowance(address(daiStrategy), F_DAI),
+            type(uint256).max
+        );
+        assertEq(
+            ERC20(USDC).allowance(address(usdcStrategy), F_USDC),
+            type(uint256).max
+        );
+        assertEq(
+            ERC20(USDT).allowance(address(usdtStrategy), F_USDT),
+            type(uint256).max
+        );
+
+        // Check underlying asset index wrt to 3pool
+        assertEq(daiStrategy.underlyingAssetIndex(), 0);
+        assertEq(usdcStrategy.underlyingAssetIndex(), 1);
+        assertEq(usdtStrategy.underlyingAssetIndex(), 2);
+
+        // Check strategy owner
+        assertEq(daiStrategy.owner(), address(this));
     }
 }
