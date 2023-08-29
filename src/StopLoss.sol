da4e5664d9de34943e9288291f184c905c2ca98d
SHAKOTN <shakotn@protonmail.com>
2023-08-29 14:00:12 +0300

    [feat] - implement basic SnL strategy for Flux

---
+++ src/StopLoss.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: AGPLv3
+pragma solidity ^0.8.13;
+
+import "./interfaces/IStop.sol";
+import "./interfaces/IStrategy.sol";
+import "./external/GVault.sol";
+import {Owned} from "../lib/solmate/src/auth/Owned.sol";
+
+contract FluxSnL is IStop, Owned {
+    GVault public gVault;
+
+    uint256 public snLThreshold = 10; // In basis points
+    uint256 public constant BPS = 10000;
+
+    constructor(address _gVault) Owned(msg.sender) {
+        gVault = GVault(_gVault);
+    }
+
+    /**
+     * @notice Flux strategy stop loss logic
+     * @dev This contract is checking if the strategy current assets are way below the snapshotted value in gVault
+     * and if true, strategy should be stopped.
+     */
+    function stopLossCheck() external view override returns (bool) {
+        IStrategy strategy = IStrategy(msg.sender);
+        // Current strategy assets
+        uint256 strategyAssets = strategy.estimatedTotalAssets();
+        (, , , uint256 totalDebt, , ) = gVault.strategies(address(strategy));
+        // If current strategy assets decreased by more than snLThreshold, return true
+        if (strategyAssets < totalDebt) {
+            uint256 diff = totalDebt - strategyAssets;
+            uint256 diffBps = (diff * BPS) / totalDebt;
+            if (diffBps > snLThreshold) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
